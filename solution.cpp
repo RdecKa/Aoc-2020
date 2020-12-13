fd65b26264d881ea9d8285cd723dfac4c6f9f1d7
Karin Frlic <kf5121@student.uni-lj.si>
2020-12-13 19:40:18 +0100

    Add solution template

---
+++ solution.cpp
@@ -0,0 +1,14 @@
+#include "aoclib.hpp"
+
+void part1(std::vector<int> &input) {}
+void part2(std::vector<int> &input) {}
+
+int main() {
+  const std::string filename = "../day-XX/input.txt";
+  auto input = aoc::readIntInput(filename);
+
+  part1(input);
+  part2(input);
+
+  return 0;
+}
