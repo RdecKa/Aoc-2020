0add96e76742d30066805a97b72c1560551e2352
Karin Frlic <kf5121@student.uni-lj.si>
2020-12-23 19:34:55 +0100

    Day 23 solution (star 2)

--- day-23/solution.cpp
+++ day-23/solution.cpp
@@ -1,21 +1,23 @@
 #include "aoclib.hpp"
 
 struct Cup {
-  const short value;
+  const uint32_t value;
   std::shared_ptr<struct Cup> next;
 
-  Cup(const short value, std::shared_ptr<struct Cup> next) : value(value), next(next) {}
+  Cup(const uint32_t value, std::shared_ptr<struct Cup> next) : value(value), next(next) {}
 };
 
 class Game {
- private:
+ protected:
   std::shared_ptr<Cup> currentCup;
+  std::vector<std::shared_ptr<Cup>> cups;
 
-  short selectDestination() {
-    short destination = currentCup->value;
+  uint32_t selectDestination() {
+    size_t numCups = this->cups.size() - 1;
+    uint32_t destination = currentCup->value;
     for (bool destinationValid = false; !destinationValid;) {
       destinationValid = true;
-      destination = ((destination - 1 + 8) % 9) + 1;
+      destination = ((destination - 1 + numCups - 1) % numCups) + 1;
       std::shared_ptr<Cup> check(this->currentCup->next);
       for (size_t i = 0; i < 3; ++i) {
         if (check->value == destination) {
@@ -32,13 +34,8 @@ class Game {
     std::shared_ptr<Cup> pickUpHead = this->currentCup->next;
     std::shared_ptr<Cup> pickUpTail = pickUpHead->next->next;
 
-    short destination = this->selectDestination();
-
-    // Find destination
-    std::shared_ptr<Cup> destinationCup(pickUpTail);
-    while (destinationCup->value != destination) {
-      destinationCup = destinationCup->next;
-    }
+    uint32_t destination = this->selectDestination();
+    std::shared_ptr<Cup> destinationCup(this->cups[destination]);
 
     // Rewire
     std::shared_ptr<Cup> afterDestinationCup(destinationCup->next);
@@ -52,10 +49,15 @@ class Game {
 
  public:
   Game(std::string& cups) {
-    auto lastCup = std::make_shared<Cup>(static_cast<short>(cups[cups.size() - 1] - '0'), nullptr);
+    this->cups = std::vector<std::shared_ptr<Cup>>(cups.size() + 1, nullptr);
+
+    auto lastCup = std::make_shared<Cup>(static_cast<uint32_t>(cups[cups.size() - 1] - '0'), nullptr);
+    this->cups[lastCup->value] = lastCup;
+
     std::shared_ptr<Cup> nextCup(lastCup);
     for (int i = cups.size() - 2; i >= 0; --i) {
-      nextCup = std::make_shared<Cup>(static_cast<short>(cups[i] - '0'), nextCup);
+      nextCup = std::make_shared<Cup>(static_cast<uint32_t>(cups[i] - '0'), nextCup);
+      this->cups[nextCup->value] = nextCup;
     }
 
     lastCup->next = nextCup;
@@ -68,14 +70,12 @@ class Game {
   void play(size_t nRounds) {
     for (size_t i = 0; i < nRounds; ++i) {
       this->playOneRound();
-      // std::cout << *this << std::endl;
     }
   }
 
   std::string getLabels() {
     // Find Cup 1
-    std::shared_ptr<Cup> cup(this->currentCup);
-    while (cup->value != 1) cup = cup->next;
+    std::shared_ptr<Cup> cup(this->cups[1]);
 
     std::string result = "";
     cup = cup->next;
@@ -89,11 +89,39 @@ class Game {
   friend std::ostream& operator<<(std::ostream& output, const Game& g) {
     output << "(" << g.currentCup->value << ") ";
     std::shared_ptr<Cup> cup = g.currentCup->next;
+    int c = 0;
     while (cup != g.currentCup) {
-      output << cup->value << " ";
+      if (c < 20 || c > 1000000 - 20) output << cup->value << " ";
       cup = cup->next;
+      ++c;
     }
     return output;
+  }
+};
+
+class BigGame : public Game {
+ public:
+  BigGame(std::string& cups) : Game(cups) {
+    uint32_t numCups = 1000000;
+    this->cups.resize(numCups + 1, nullptr);
+
+    std::shared_ptr<Cup> lastCup = this->currentCup;
+    while (lastCup->next != this->currentCup) lastCup = lastCup->next;
+
+    std::shared_ptr<Cup> nextCup(this->currentCup);
+    for (uint32_t cupNum = numCups; cupNum >= 10; --cupNum) {
+      nextCup = std::make_shared<Cup>(cupNum, nextCup);
+      this->cups[cupNum] = nextCup;
+    }
+
+    lastCup->next = nextCup;
+  }
+
+  unsigned long getResult() {
+    std::shared_ptr<Cup> first = this->cups[1]->next;
+    std::shared_ptr<Cup> second = first->next;
+
+    return static_cast<unsigned long>(first->value) * static_cast<unsigned long>(second->value);
   }
 };
 
@@ -103,7 +131,11 @@ void part1(std::string& input) {
   std::cout << game.getLabels() << std::endl;
 }
 
-void part2(std::string& input) {}
+void part2(std::string& input) {
+  BigGame game(input);
+  game.play(10000000);
+  std::cout << game.getResult() << std::endl;
+}
 
 int main() {
   std::string input = "942387615";  // My input
